7bedab1943f638268da54cc53481fef7c725dc20
nimit3 <chavdanimit@gmail.com>
2020-06-29 22:21:16 -0400

    tour page without map locations of tour

--- controllers/viewsController.js
+++ controllers/viewsController.js
@@ -8,15 +8,24 @@ exports.getOverview = catchAsync(async (req, res, next) => {
   // 2) BUild template of tour data
 
   // 3) render that template using tour data from 1 step
-
   res.status(200).render('overview', {
     title: 'All Tours',
     tours,
   });
 });
 
-exports.getTour = (req, res) => {
+exports.getTour = catchAsync(async (req, res) => {
+  //1) Get that data, for the requested tour (including reviews and guides)
+  const tour = await Tour.findOne({ slug: req.params.slug }).populate({
+    path: 'reviews',
+    flields: 'review rating user',
+  });
+  //2) Build template
+
+  // 3) Render template
+
   res.status(200).render('tour', {
     title: 'the Forest Hiker Tour',
+    tour,
   });
-};
+});
